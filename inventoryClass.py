f0362874fdef8f66d9a065981bac31831cd89168
zj <alialzragi2324@gmail.com>
2022-08-03 03:53:21 -0400

    created layout and item class

---
+++ inventoryClass.py
@@ -0,0 +1,10 @@
+class item:
+    def __init__(self,name,expDate,itemType,sellingPrice,cost, numUnits):
+        self.name = name
+        self.expDate = expDate
+        self.itemType = itemType
+        self.sellingPrice = sellingPrice
+        self.cost = cost
+        self.Units = numUnits
+
+
--- main.py
+++ main.py
@@ -1,2 +1,25 @@
 # Project Name: Inventory System
-# Name: 
+# Name: Alzraji Abdulla-Ali
+# Date Created: 8-03-2022 
+# Purpose: First python project, program will act as an inventory system for a small convience store
+
+import inventoryClass
+
+def addItem():
+    NewItemName = input("Enter item name: ")
+    NewItemExp = input("Enter experation Date")
+    NewItemType = input("Enter type of Item (Grocey, tobaco, or taxable): ")
+    NewItemPrice = input("Enter the selling price: ")
+    NewItemCost = input("Cost of the product: ")
+    newItemUnits = input("how many units being added: ")
+    AnewItem = inventoryClass.item(NewItemName, NewItemExp, NewItemType, NewItemCost, newItemUnits)
+    
+
+#print("Welcome to your Inventory Control System")
+
+#print("What would you like to do: ")
+#print("A: Add an item")
+#print("B: Remove an item")
+#print("C: search for an item inventory")
+#print("D: Print Inventory sheet")
+addItem()
