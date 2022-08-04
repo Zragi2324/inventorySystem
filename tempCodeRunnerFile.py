e098aa99223bbb70a07b4b80985ee6b925f65934
zj <alialzragi2324@gmail.com>
2022-08-04 04:24:43 -0400

    working on fixing delete func

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -3,11 +3,12 @@
 # Date Created: 8-03-2022 
 # Purpose: First python project, program will act as an inventory system for a small convience store
 
+from os import remove
 import inventoryClass
 
+list = []
 
-
-def addItem (list = []): 
+def addItem (list): 
     NewItemName = input("Enter item name: ")
     NewItemExp = input("Enter experation Date: ")
     NewItemType = input("Enter type of Item (Grocey, tobaco, or taxable): ")
@@ -17,21 +18,27 @@ def addItem (list = []):
     list.append ( inventoryClass.item(NewItemName, NewItemExp, NewItemType,NewItemPrice, NewItemCost, newItemUnits) )
     
 
-
+def removeItem(list):
+    deleteItem = input("Enter the name of the item you want to remove")
+    for index, x in enumerate(list):
+        list.remove(deleteItem)
    
 
-#print("Welcome to your Inventory Control System")
+print("******************** Welcome to your Inventory Control System ********************")
 
-#print("What would you like to do: ")
-#print("A: Add an item")
+print("What would you like to do? Choose one of the following: ")
+print("A: Add an item")
 #print("B: Remove an item")
 #print("C: search for an item inventory")
 #print("D: Print Inventory sheet")
 
 newList = []
 addItem(newList)
+#addItem(newList)
 for obj in newList:
     print(obj)
+
+removeItem(newList)
 
 
 
