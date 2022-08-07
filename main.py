1d233cf86a0f5594ef955d6fabd896d2025f05a4
zj <alialzragi2324@gmail.com>
2022-08-07 03:06:28 -0400

    completed methods, work on clean up and error handeling

--- main.py
+++ main.py
@@ -4,13 +4,14 @@
 # Purpose: First python project, program will act as an inventory system for a small convience store
 
 
+from fileinput import filename
 import inventoryClass
 
 # created a list for to work as parameter 
 list = []
 
 #FIXme: file not being read & written correctly delete and try again. Parameteers incoirrect for file 
-def addItem (list, file ): 
+def addItem (list): 
     NewItemName = input("Enter item name: ")
     NewItemExp = input("Enter experation Date: ")
     NewItemType = input("Enter type of Item (Grocey, tobaco, or taxable): ")
@@ -18,12 +19,9 @@ def addItem (list, file ):
     NewItemCost = input("Cost of the product: ")
     newItemUnits = input("how many units being added: ")
     list.append ( inventoryClass.item(NewItemName, NewItemExp, NewItemType,NewItemPrice, NewItemCost, newItemUnits) )
-    file = open("Inventory Sheet", "x")
-    file.write(print(list))
-    file.close()
 
-def removeItem(list, file):
-    file = open("Inventory Sheet", "w")
+def removeItem(list):
+    file = open("Inventory Sheet", "a")
     if len(list) == 0:
         print("Nothing to remove, no inventory to remove.\n")
         return
@@ -31,11 +29,11 @@ def removeItem(list, file):
     for i in range (len(list)):
         if list[i].name == deleteItem:
             del list[i]
-    file.replace(deleteItem, "")
-    file.close()
+    
 
 def searchItem(list, target):
     
+    target = input("enter name of item to search for : ")
     for i in range(len(list)):
         if list[i].name == target:
             print(list[i])
@@ -47,12 +45,16 @@ def searchItem(list, target):
 def printInventory(list):
     for obj in list:
         print(obj)
+    with open("Inventory Sheet.txt", "w+") as fileName:
+        for items in list:
+            fileName.write('%s\n'%items)
+    fileName.close()
 
-def switch(case, list, target, file):
+def switch(case, list, target):
     if (case == 'A') or (case =='a'):
-        addItem(list,file)
+        addItem(list)
     elif (case == 'b') or (case == 'B'):
-        removeItem(list,file)
+        removeItem(list)
     elif (case == 'c') or (case == 'C'):
         searchItem(list, target)
     elif (case == 'd') or (case == 'D'):
@@ -63,9 +65,8 @@ def switch(case, list, target, file):
 
 #acts as user input for choice
 quitChar = 'o'
-newTarget = "searchItem"
+newTarget = ""
 newList = []
-newFile = open("Inventor Sheet.txt", "x")
 
 print("******************** Welcome to your Inventory Control System ********************\n")
 while not (quitChar == 'q' or  quitChar == 'Q'):
@@ -77,5 +78,5 @@ while not (quitChar == 'q' or  quitChar == 'Q'):
     print("D: Print Inventory sheet\n")
 
     quitChar = input("\nchoose an option: ")
-    switch(quitChar, newList,newTarget, newFile)
-    newFile.close()
+    switch(quitChar, newList,newTarget)
+    
